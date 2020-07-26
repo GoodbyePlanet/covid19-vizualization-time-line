204da5d8282cae0226f739d4d7f636b06a0e396a
Nemanja <nemanja.vasic@codecentric.de>
2020-07-26 15:07:52 +0200

    Reset graph after race is done

--- src/index.js
+++ src/index.js
@@ -49,20 +49,22 @@ const REQUEST_PARAM = "?lastdays=-1"; // From API docs (use -1 to get all data)
 async function getLastDate() {
   const data = await getCases();
   const cases = data[0].cases;
-  const date = cases[cases.length - 1].date;
+  const date = Object.keys(cases);
 
-  return new Date(date);
+  return new Date(date[date.length - 1]);
 }
 
 async function getCases() {
   const data = await getCountriesHistoricalData();
 
-  return data
-    .map((country) => toObject(country))
+  return data.map((country) => toObject(country));
 }
 
 function toObject(country) {
-  return { countryName: country.data.country, cases: country.data.timeline.cases };
+  return {
+    countryName: country.data.country,
+    cases: country.data.timeline.cases,
+  };
 }
 
 function getCountriesHistoricalData() {
@@ -99,101 +101,97 @@ async function getAllCasesForContry(country) {
   }
 }
 
-
 //     const lastDate = await getLastDate();
 //     if (startDate > lastDate) {
 //       startDate = new Date("2/25/20");
 //     }
 
-// if (dateFormat(startDate, "m/d/yy") === "1/23/2020") 
-
+// if (dateFormat(startDate, "m/d/yy") === "1/23/2020")
 
 // HIGH CHARTS
 
+
+const startingDate = '2/25/20';
 let startDate = new Date("2/25/20");
 
-var initialData,
-  chart;
-const startYear = dateFormat(startDate, "m/d/yy"),
+var initialData, chart;
+let startYear = dateFormat(startDate, "m/d/yy"),
   endYear = 2018,
-  btn = document.getElementById('play-pause-button'),
-  input = document.getElementById('play-range');
+  btn = document.getElementById("play-pause-button"),
+  input = document.getElementById("play-range");
 
 /**
  * Animate dataLabels functionality
  */
-(function(H) {
+(function (H) {
   const FLOAT = /^-?\d+\.?\d*$/;
 
   // Add animated textSetter, just like fill/strokeSetters
-  H.Fx.prototype.textSetter = function(proceed) {
-    var startValue = this.start.replace(/ /g, ''),
-      endValue = this.end.replace(/ /g, ''),
-      currentValue = this.end.replace(/ /g, '');
+  H.Fx.prototype.textSetter = function (proceed) {
+    var startValue = this.start.replace(/ /g, ""),
+      endValue = this.end.replace(/ /g, ""),
+      currentValue = this.end.replace(/ /g, "");
 
-    if ((startValue || '').match(FLOAT)) {
+    if ((startValue || "").match(FLOAT)) {
       startValue = parseInt(startValue, 10);
       endValue = parseInt(endValue, 10);
 
       // No support for float
       currentValue = Highcharts.numberFormat(
-        Math.round(startValue + (endValue - startValue) * this.pos), 0);
+        Math.round(startValue + (endValue - startValue) * this.pos),
+        0
+      );
     }
 
     this.elem.endText = this.end;
 
-    this.elem.attr(
-      this.prop,
-      currentValue,
-      null,
-      true
-    );
+    this.elem.attr(this.prop, currentValue, null, true);
   };
 
   // Add textGetter, not supported at all at this moment:
-  H.SVGElement.prototype.textGetter = function(hash, elem) {
-    var ct = this.text.element.textContent || '';
+  H.SVGElement.prototype.textGetter = function (hash, elem) {
+    var ct = this.text.element.textContent || "";
     return this.endText ? this.endText : ct.substring(0, ct.length / 2);
-  }
+  };
 
   // Temporary change label.attr() with label.animate():
   // In core it's simple change attr(...) => animate(...) for text prop
-  H.wrap(H.Series.prototype, 'drawDataLabels', function(proceed) {
+  H.wrap(H.Series.prototype, "drawDataLabels", function (proceed) {
     var ret,
       attr = H.SVGElement.prototype.attr,
       chart = this.chart;
 
     if (chart.sequenceTimer) {
-      this.points.forEach(
-        point => (point.dataLabels || []).forEach(
-          label => label.attr = function(hash, val) {
-            if (hash && hash.text !== undefined) {
-              var text = hash.text;
+      this.points.forEach((point) =>
+        (point.dataLabels || []).forEach(
+          (label) =>
+            (label.attr = function (hash, val) {
+              if (hash && hash.text !== undefined) {
+                var text = hash.text;
 
-              delete hash.text;
+                delete hash.text;
 
-              this.attr(hash);
+                this.attr(hash);
 
-              this.animate({
-                text: text
-              });
-              return this;
-            } else {
-              return attr.apply(this, arguments);
-            }
-          }
+                this.animate({
+                  text: text,
+                });
+                return this;
+              } else {
+                return attr.apply(this, arguments);
+              }
+            })
         )
       );
     }
 
     ret = proceed.apply(this, Array.prototype.slice.call(arguments, 1));
 
-    this.points.forEach(
-      p => (p.dataLabels || []).forEach(d => d.attr = attr)
+    this.points.forEach((p) =>
+      (p.dataLabels || []).forEach((d) => (d.attr = attr))
     );
 
     return ret;
-
   });
 })(Highcharts);
 
@@ -202,28 +200,37 @@ const startYear = dateFormat(startDate, "m/d/yy"),
  */
 
 function getData(year) {
-  let output = initialData.map(data => {
-    return [data["countryName"], data.cases[year]]
-  }).sort((a, b) => b[1] - a[1]);
+  let output = initialData
+    .map((data) => {
+      return [data["countryName"], data.cases[year]];
+    })
+    .sort((a, b) => b[1] - a[1]);
 
-  return ([output[0], output]);
+  return [output[0], output];
 }
 
-Highcharts.getJSON('https://api.npoint.io/d70fd8f47a70326609bb', async function (data) {
+Highcharts.getJSON(
+  "https://api.npoint.io/d70fd8f47a70326609bb",
+  async function (data) {
     initialData = await getCases();
 
-		  chart = Highcharts.chart('container', {
+    chart = Highcharts.chart("container", {
       chart: {
         animation: {
-          duration: 500
+          duration: 500,
         },
         events: {
           render() {
             let chart = this;
 
             // Responsive input
-            input.style.width = chart.plotWidth - chart.legend.legendWidth + chart.plotLeft / 2 - 10 + 'px'// where 10 is a padding
-          }
+            input.style.width =
+              chart.plotWidth -
+              chart.legend.legendWidth +
+              chart.plotLeft / 2 -
+              10 +
+              "px"; // where 10 is a padding
+          },
         },
         marginRight: 50,
       },
@@ -232,118 +239,129 @@ Highcharts.getJSON('https://api.npoint.io/d70fd8f47a70326609bb', async function 
           animation: false,
           groupPadding: 0,
           pointPadding: 0.1,
-          borderWidth: 0
-        }
+          borderWidth: 0,
+        },
       },
       title: {
         useHTML: true,
-        text: `World population - overall: <b>${getData(startYear)[0][1]}</b>`
+        text: `World population - overall: <b>${getData(startYear)[0][1]}</b>`,
       },
 
       legend: {
-        align: 'right',
-        verticalAlign: 'bottom',
+        align: "right",
+        verticalAlign: "bottom",
         itemStyle: {
-          fontWeight: 'bold',
-          fontSize: '50px',
+          fontWeight: "bold",
+          fontSize: "50px",
         },
         symbolHeight: 0.001,
         symbolWidth: 0.001,
         symbolRadius: 0.001,
       },
       xAxis: {
-        type: 'category',
+        type: "category",
       },
-      yAxis: [{
-        opposite: true,
-        title: {
-          text: 'Population per country'
+      yAxis: [
+        {
+          opposite: true,
+          title: {
+            text: "Population per country",
+          },
+          tickAmount: 5,
         },
-        tickAmount: 5
-      }],
-      series: [{
-        colorByPoint: true,
-        dataSorting: {
-          enabled: true,
-          matchByName: true
+      ],
+      series: [
+        {
+          colorByPoint: true,
+          dataSorting: {
+            enabled: true,
+            matchByName: true,
+          },
+          type: "bar",
+          dataLabels: [
+            {
+              enabled: true,
+            },
+          ],
+          name: startYear,
+          data: getData(startYear)[1],
         },
-        type: 'bar',
-        dataLabels: [{
-          enabled: true,
-        }],
-        name: startYear,
-        data: getData(startYear)[1]
-      }]
+      ],
     });
-});
+  }
+);
 
 /**
  * Update the chart. This happens either on updating (moving) the range input,
  * or from a timer when the timeline is playing.
  */
-function update(increment) {
-
-  // console.log('INPUT.VALUE ', input.value)
+async function update(increment) {
+  const endDate = await getLastDate();
 
   if (increment) {
-    // input.value = parseInt(input.value) + increment;
-    input.value = dateFormat(new Date(startDate.setDate(startDate.getDate() + 1)), 'm/d/yy');
+    input.value = dateFormat(
+      new Date(startDate.setDate(startDate.getDate() + 1)),
+      "m/d/yy"
+    );
   }
-  if (input.value >= endYear) { // Auto-pause
+  if (new Date(input.value) >= endDate) {
+    input.value = startingDate;
+    startDate = new Date("2/25/20");
     pause(btn);
   }
 
-  chart.update({
-    title: {
-      useHTML: true,
-      text: `<div>World population - overall: <b>${getData(input.value)[0][1]}</b></span></div>`
+  chart.update(
+    {
+      title: {
+        useHTML: true,
+        text: `<div>World population - overall: <b>${
+          getData(input.value)[0][1]
+        }</b></span></div>`,
+      },
     },
-  }, false, false, false)
-
-  console.log('GET DATA ', getData(input.value));
+    false,
+    false,
+    false
+  );
 
   chart.series[0].update({
     name: input.value,
-    data: getData(input.value)[1]
-  })
+    data: getData(input.value)[1],
+  });
 }
 
 /**
  * Play the timeline.
  */
 function play(button) {
-  button.title = 'pause';
-  button.className = 'fa fa-pause';
-  chart.sequenceTimer = setInterval(function() {
-    update(1);
+  button.title = "pause";
+  button.className = "fa fa-pause";
+  chart.sequenceTimer = setInterval(async function () {
+    await update(1);
   }, 500);
-
 }
 
-/** 
+/**
  * Pause the timeline, either when the range is ended, or when clicking the pause button.
  * Pausing stops the timer and resets the button to play mode.
  */
 function pause(button) {
-  button.title = 'play';
-  button.className = 'fa fa-play';
+  button.title = "play";
+  button.className = "fa fa-play";
   clearTimeout(chart.sequenceTimer);
   chart.sequenceTimer = undefined;
 }
 
-
-btn.addEventListener('click', function() {
+btn.addEventListener("click", function () {
   if (chart.sequenceTimer) {
-    pause(this)
+    pause(this);
   } else {
-    play(this)
+    play(this);
   }
-})
-/** 
+});
+/**
  * Trigger the update on the range bar click.
  */
-input.addEventListener('click', function() {
-  update()
-})
-
-
+input.addEventListener("click", function () {
+  update();
+});
